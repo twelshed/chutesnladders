eb66debadafca504acfd40a628e7f54ca3105361
andrewatson <andre.watson@gmail.com>
2021-05-28 17:59:59 -0700

    Proper Edge Bouncing

--- boundaries.py
+++ boundaries.py
@@ -23,7 +23,6 @@ class BrownianParticle():
         self.pos_hist[0,:] = np.array([xpos,ypos])
 
     def step(self, dt, n):
-        n = int(n)
         x0 = self.pos_hist[self.curr_iter,:]
 
         # For each element of x0, generate a sample of n numbers from a
@@ -36,12 +35,8 @@ class BrownianParticle():
         r[1,:] = r[1,:] + drift
 
         out = np.empty(r.shape)
-        #np.cumsum(r, axis=-1, out=out)
         out += np.expand_dims(x0, axis=-1)
-      
-        #out = self.rule_check(out)
         out = self.applyValues(r,out)
-        print(out)
         
         self.pos_hist[self.curr_iter:self.curr_iter+n,:] = out.T
         self.curr_iter += n
@@ -59,57 +54,22 @@ class BrownianParticle():
         out[0,0] = self.x
         out[1,0] = self.y  
         
-        for i in range(1, self.pos_hist.shape[0]):
-            #stick on edge and "bounce"
-            if out[0,i-1]==bounds[0]:
-                if r[0,i]<0:
-                    r[0,i]*=-1
-            if out[0,i]==bounds[1]:
-                if r[0,i]>0:
-                    r[0,i]*=-1  
-            if out[1,i]==bounds[2]:
-                if r[0,i]<0:
-                    r[0,i]*=-1  
-            if out[1,i]==bounds[3]:    
-                if r[0,i]>0:
-                    r[0,i]*=-1      
-          
-            # # more aggresive attempt, don't allow movement that will hit edge
-            # if (r[0,i] + out[0,i-1])<bounds[0]:
-                # if r[0,i]<0:
-                    # r[0,i]*=-1
-            # if (r[0,i] + out[0,i-1])>bounds[1]:
-                # if r[0,i]>0:
-                    # r[0,i]*=-1  
-            # if (r[1,i] + out[1,i-1])<bounds[2]:
-                # if r[0,i]<0:
-                    # r[0,i]*=-1  
-            # if (r[1,i] + out[1,i-1])>bounds[3]:    
-                # if r[0,i]>0:
-                    # r[0,i]*=-1      
+        for i in range(1, self.pos_hist.shape[0]): 
+            # proper bounce
+            if (r[0,i] + out[0,i-1])<bounds[0]:
+                #print("inc r "+str(r[0,i])+" out "+str(out[0,i-1]))
+                r[0,i]= -(out[0,i-1] - bounds[0] + (r[0,i] + out[0,i-1] - bounds[0]))
+                #print("new r "+str(r[0,i])+" out "+str(out[0,i-1]))
+            if (r[0,i] + out[0,i-1])>bounds[1]:
+                r[0,i]= -(out[0,i-1] - bounds[1] + (r[0,i] + out[0,i-1] - bounds[1])) 
+            if (r[1,i] + out[1,i-1])<bounds[2]:
+                r[1,i]= -(out[1,i-1] - bounds[2] + (r[1,i] + out[1,i-1] - bounds[2])) 
+            if (r[1,i] + out[1,i-1])>bounds[3]:    
+                r[1,i]= -(out[1,i-1] - bounds[3] + (r[1,i] + out[1,i-1] - bounds[3]))       
           
             out[0,i] = r[0,i] + out[0,i-1]
             out[1,i] = r[1,i] + out[1,i-1] 
-            
-            if out[0,i]<bounds[0]:
-                out[0,i] = bounds[0]
-            if out[0,i]>bounds[1]:
-                out[0,i] = bounds[1]
-            if out[1,i]<bounds[2]:
-                out[1,i] = bounds[2]
-            if out[1,i]>bounds[3]:
-                out[1,i] = bounds[3]
                 
         return out
                 
-    def rule_check(self, out):
-        # truncates any movement out of bounds, currently does it across the whole history
-        # should only do this when particle moves out of bounds then recalc, but for now 
-        # less stupid as n approaches 1, valid for n=1
-        bounds = self.env_tuple
-        out[0,out[0,:]<bounds[0]] = bounds[0]
-        out[0,out[0,:]>bounds[1]] = bounds[1]
-        out[1,out[1,:]<bounds[2]] = bounds[2]
-        out[1,out[1,:]>bounds[3]] = bounds[3]
-        return out
         
--- run.py
+++ run.py
@@ -5,24 +5,34 @@ from utils import pos_CDFs, save_hists, CDF_gif
 
 
 if __name__ == "__main__":
-    delta = .25
-    T = 30
-    N = 100
-    dt = T/N
-    n_parts = 100
-    alpha = 0.025
-    n_steps = 10
-    gif_path = 'grav/'
+    delta = .25         # average walk length
+    T = 30              # time in seconds
+    N = 100             # overall iterations
+    dt = T/N            # delta time per iteration
+    n_parts = 10        # number of particles
+    alpha = 0.025       # drag coef
+    n_steps = 10        # snap shot iterator
+    gif_path = 'grav/'  # animated gif output directory, must be present
 
-    bps = [BrownianParticle(2+np.random.rand(),15+np.random.rand(),N/dt, alpha = alpha) for i in range(n_parts)]
-    for i in range(n_steps):
-        for bp in bps:
-            bp.step(dt, N/dt)
+    bps = [BrownianParticle(2+np.random.rand(),15+np.random.rand(),N, alpha = alpha, delta = delta) for i in range(n_parts)]
+    
+    
+    # for i in range(n_steps):
+        # for bp in bps:
+            # bp.step(dt, N)
 
-        unstuck = pos_CDFs(bps, show=False)
-        save_hists(unstuck, i, gif_path)
-        [bp.reset() for bp in bps]
+        # unstuck = pos_CDFs(bps, show=False)
+        # save_hists(unstuck, i, gif_path)
+        # [bp.reset() for bp in bps]
 
-    CDF_gif(gif_path, 'grav_gif.gif')
-    #ppp(bps)
+    # CDF_gif(gif_path, 'grav_gif.gif')
+    
+    
+    
+    for bp in bps:
+        bp.step(dt, N)
+    ppp(bps)
+    
+    
+    
     #pos_CDFs(bps)
