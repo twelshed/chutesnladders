a698cda8eaf00ad4d1ebfc7f5053f62ca460c948
LAPTOP-D3A6OIC2\andre <andre.watson@gmail.com>
2021-11-28 20:04:43 -0600

    test initial params

--- Config.py
+++ Config.py
@@ -2,8 +2,10 @@ class Config:
     delta = 1         # average walk length
     T = 100              # time in seconds
     N = 100             # overall iterations
+    TNStep = 50
     dt = T/N            # delta time per iteration
-    sigma = 2e-5 #2e-9
+    sigma = 1e-5 #2e-9
+    sigmaStep = 1e-5 #2e-9
     n_parts = 1000         # number of particles
     alpha = 0.00       # drag coef
     n_steps = 10000        # snap shot iterator
--- run.py
+++ run.py
@@ -14,65 +14,72 @@ if __name__ == "__main__":
     #bps = [BrownianParticle(Config, np.random.rand(),Config.env_tuple[3]/2+np.random.rand()) for i in range(Config.n_parts)]
 
     import time
-    st = time.time()
     p = Pool(Config.workers)
     # sigma = dt/n_steps
     # x = sigma * np.sqrt(np.arange(30000)/np.pi) + np.sqrt(delta*dt)
     # xi = .33 * x
     # plt.fill_between(np.arange(30000), (x-xi), (x+xi), color= 'b', alpha=.1)
 
-    for i in range(Config.ensembles):
-        dnoise = Config.sigma*100
+    for t in range(7):
+        Config.sigma = Config.sigmaStep
+        
+        for s in range(20):
+            st = time.time()
+            
+            dnoise = Config.sigma*100
 
-        bps = [BrownianParticle(Config,
-                                Config.env_tuple[1]/2+np.random.randn()*dnoise,
-                                Config.env_tuple[3]/2+np.random.randn()*dnoise) 
-                                for i in range(Config.n_parts)]
+            bps = [BrownianParticle(Config,
+                                    Config.env_tuple[1]/2,
+                                    Config.env_tuple[3]/2) 
+                                    for i in range(Config.n_parts)]
 
-        bps = p.map(run_index, bps)
-
-
-        # exp_r=np.zeros(n_steps)
-        # for i in range(n_steps):
-
-        #     for bp in bps:
-        #         bp.step(dt, N)
-
-        #     #stuck, unstuck = pos_CDFs(bps, show=False)
-        #     #save_hists(unstuck, i, gif_path, stuck = stuck, stacked=True)
-        #     exp_r[i] = expectation_radius(bps, n_parts,center=[5,5])
-
-        #     [bp.reset() for bp in bps]
-
-        #CDF_gif(gif_path, 'grav_gif.gif')
-        #exp_r = np.zeros(len(bps[0].exp_r))
-        avgp = [bp.avg_pos for bp in bps]
-      
-        avgp = np.vstack(avgp)
-
-        plt.hist2d(avgp[:,0], avgp[:,1], bins=(300, 300),range=[[0,Config.env_tuple[1]],[0,Config.env_tuple[3]]], cmap=plt.cm.jet)
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.title(f'Particle displacement \n Random Walks : {Config.n_steps*Config.N} \n delta = {Config.delta} \n Positional Noise = {dnoise}')
-        plt.colorbar()
-        plt.savefig(f'avgp_hist{Config.n_steps*Config.N}.png')
-        plt.close()
+            bps = p.map(run_index, bps)
 
 
+            # exp_r=np.zeros(n_steps)
+            # for i in range(n_steps):
 
-        exp_r = [bp.exp_r for bp in bps]
-        exp_r = np.asarray(exp_r)
-        exp_r = np.sum(exp_r,axis=0)/Config.n_parts
+            #     for bp in bps:
+            #         bp.step(dt, N)
 
-        print(f'Took:{time.time()-st}')
-        plt.plot(list(range(Config.n_steps)),exp_r)
-        plt.xlabel(f'Random Walks : {Config.n_steps*Config.N}')
-        plt.ylabel('<r^2>')
-        plt.title(f'Expected radius N_Particles = {Config.n_parts} \n delta = {Config.delta} \n Positional Noise = {dnoise}')
-        plt.savefig(f'exp_radius{Config.n_steps*Config.N}.png')
+            #     #stuck, unstuck = pos_CDFs(bps, show=False)
+            #     #save_hists(unstuck, i, gif_path, stuck = stuck, stacked=True)
+            #     exp_r[i] = expectation_radius(bps, n_parts,center=[5,5])
+
+            #     [bp.reset() for bp in bps]
+
+            #CDF_gif(gif_path, 'grav_gif.gif')
+            #exp_r = np.zeros(len(bps[0].exp_r))
+            avgp = [bp.avg_pos for bp in bps]
+          
+            avgp = np.vstack(avgp)
+
+            plt.hist2d(avgp[:,0], avgp[:,1], bins=(300, 300),range=[[0,Config.env_tuple[1]],[0,Config.env_tuple[3]]], cmap=plt.cm.jet)
+            plt.xlabel('x')
+            plt.ylabel('y')
+            plt.title(f'Particle displacement \n Random Walks : {Config.n_steps*Config.N} \n delta = {Config.delta} \n Positional Noise = {dnoise}')
+            plt.colorbar()
+            plt.savefig(f'avgp_hist{Config.env_tuple[1]*100}_{Config.N}_{Config.sigma}.png')
+            plt.close()
+
+
+
+            # exp_r = [bp.exp_r for bp in bps]
+            # exp_r = np.asarray(exp_r)
+            # exp_r = np.sum(exp_r,axis=0)/Config.n_parts
+
+            print(f'T={Config.T} N={Config.N} Sigma={Config.sigma} Took:{time.time()-st}')
+            # plt.plot(list(range(Config.n_steps)),exp_r)
+            # plt.xlabel(f'Random Walks : {Config.n_steps*Config.N}')
+            # plt.ylabel('<r^2>')
+            # plt.title(f'Expected radius N_Particles = {Config.n_parts} \n delta = {Config.delta} \n Positional Noise = {dnoise}')
+            # plt.savefig(f'exp_radius{Config.n_steps*Config.N}.png')
    
-    
-    
+            Config.sigma += Config.sigmaStep
+            
+        Config.T += Config.TNStep
+        Config.N += Config.TNStep
+        
     # for bp in bps:
        # bp.step(Config.dt, Config.N)
     # ppp(bps)
